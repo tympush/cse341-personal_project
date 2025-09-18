21e2357b86dac386d916aff463a2c15b65882b77
tympush <tymur.pushnoy@gmail.com>
2025-09-18 19:04:57 +0200

    added swagger

---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Contact API',
+        description: 'Contact API',
+    },
+    host: 'localhost:8080',
+    schemes: ['http', 'https']
+};
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
