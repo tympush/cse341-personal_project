c9fc4126807c91b4ace1e6be8576eea1b49270da
tympush <tymur.pushnoy@gmail.com>
2025-09-18 23:35:01 +0200

    failed to fetch potential fix

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const mongodb = require("./db/connect");
 const port = process.env.PORT || 8080;
 const server = express();
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 server.use(bodyParser.json());
 
 const mainRoutes = require("./routes/index");
