0f17c98def068fe0cb51b7d38a224166301632dd
tympush <tymur.pushnoy@gmail.com>
2025-09-18 23:12:06 +0200

    added validation to route

--- routes/weapons.js
+++ routes/weapons.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const weaponsController = require("../controllers/weapons");
-//const validation = require("../middleware/validate");
+const validation = require("../middleware/validate");
 
 router.get("/", weaponsController.getAll);
 
 router.get("/:id", weaponsController.getSingle);
 
-router.post("/", weaponsController.createWeapon);
+router.post("/", validation.saveWeapon, weaponsController.createWeapon);
 
-router.put("/:id", weaponsController.updateWeapon);
+router.put("/:id", validation.saveWeapon, weaponsController.updateWeapon);
 
 router.delete("/:id", weaponsController.deleteWeapon);
 
