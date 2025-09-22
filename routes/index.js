9bbb059f88613ed8fb8784f264e16f630bc62d62
tympush <tymur.pushnoy@gmail.com>
2025-09-22 14:19:13 +0200

    added authentication (not tested)

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const passport = require("passport");
+
 const router = require("express").Router();
 
 router.use("/", require("./swagger"));
@@ -8,5 +10,14 @@ router.get("/", (req, res) => {
 
 router.use("/weapons", require("./weapons"));
 router.use("/enemies", require("./enemies"));
+
+router.get("/login", passport.authenticate("github", (req, res) => {}));
+
+router.get("/logout", function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
